af72431156e4abcfabdc32d4d4e770c5ffdf7b99
Diego Forganes <diforg@gmail.com>
2026-04-11 15:03:44 -0300

    test: add Transfer and Recurrence model feature tests

--- src/app/Models/Recurrence.php
+++ src/app/Models/Recurrence.php
@@ -3,11 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Recurrence extends Model
 {
+    use HasFactory;
     use SoftDeletes;
 
     public const FREQUENCIES = [
